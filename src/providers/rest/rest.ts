f20499979392a0a42a304e66fba3c84b27ece163
jamenesesg1 <jamenesesg1@gmail.com>
2018-07-16 18:20:03 -0500

    Primer_Commit

---
+++ src/providers/rest/rest.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+@Injectable()
+export class RestProvider {
+
+  apiUrl = 'https://jsonplaceholder.typicode.com';
+
+  constructor(public http: HttpClient) {
+    console.log('Hello RestProvider Provider');
+  }
+
+  getUsers() {
+    return new Promise(resolve => {
+      this.http.get(this.apiUrl+'/users').subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+  addUser(data) {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.apiUrl+'/users', JSON.stringify(data))
+        .subscribe(res => {
+          resolve(res);
+        }, (err) => {
+          reject(err);
+        });
+    });
+  }
+
+}
